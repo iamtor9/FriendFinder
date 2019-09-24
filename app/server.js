741ee59cb356c283e33de8cadbcfeeb94ed10f34
iamtor9 <iamtor9@gmail.com>
2019-09-24 17:27:13 -0500

    server.js DONE

--- app/server.js
+++ app/server.js
@@ -6,104 +6,32 @@ const path = require("path");
 
 //create express app function and port
 const app = express();
-const PORT = 8889;
 
+const PORT = process.env.PORT || 8080;
 
-//this needs to be changed... pretty sure i fucked it up already but i am going to keep going.
-//const app = app();
-
-//app.set('view engine', 'app');
-//app.set('app',path.join(__dirname, 'app'));
-
-
-// Sets up the Express app to handle data parsing
+// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// direct server to the route files 
+//handlebars handle
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
-//create listener to start server a.k.a listen for "PORT"
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
-
-
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  // res.send("Welcome to the Star Wars Page!")
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
-
-// Displays all characters
-app.get("/api/friends.js", function(req, res) {
-  return res.json(friends.js);
-});
-
-//----------------------------------------------
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
-
-  console.log(newcharacter);
-
-  // We then add the json the user sent to the character array
-  characters.push(newcharacter);
-
-  // We then display the JSON to the users
-  res.json(newcharacter);
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
+// Starts the server to begin listening (using arrow function):
+app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
 });
+
+// // routes
+// const routes = require("./routing/friends.js");
+
+// app.use(routes);
+
+// // Start our server so that it can begin listening to client requests.
+// app.listen(PORT, function() {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http://localhost:" + PORT);
+// });
